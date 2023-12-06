0aff7db4d61ed74b03d5e27bb572151249b4ba48
Joseph Finney <TheJoeFin@users.noreply.github.com>
2023-12-06 17:11:21 -0600

    File scoped name spaces and clean up usings

--- ModernStandbyFix/MainWindow.xaml.cs
+++ ModernStandbyFix/MainWindow.xaml.cs
@@ -1,55 +1,37 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ModernStandbyFix
+namespace ModernStandbyFix;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            Initialize();
-        }
+        InitializeComponent();
+        Initialize();
+    }
 
-        private void Initialize()
-        {
-            versionTextBlock.Text = "Version: " + Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString();
-            runOnStartupCheckBox.IsChecked = RegistryUtils.IsRunningOnStartup(App.GetApplicationPath());
-            runOnStartupCheckBox.Checked += runOnStartupCheckBox_Checked;
-            runOnStartupCheckBox.Unchecked += runOnStartupCheckBox_Checked;
-        }
+    private void Initialize()
+    {
+        versionTextBlock.Text = "Version: " + Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString();
+        runOnStartupCheckBox.IsChecked = TaskSchedulerUtils.IsRunningOnStartup(App.GetApplicationPath());
+        runOnStartupCheckBox.Checked += runOnStartupCheckBox_Checked;
+        runOnStartupCheckBox.Unchecked += runOnStartupCheckBox_Checked;
+    }
 
-        private void runOnStartupCheckBox_Checked(object sender, RoutedEventArgs e)
+    private void runOnStartupCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+        if (TaskSchedulerUtils.ToggleRunOnStartup(App.GetApplicationPath()))
         {
-            if (RegistryUtils.ToggleRunOnStartup(App.GetApplicationPath()))
-            {
-                runOnStartupCheckBox.IsChecked = RegistryUtils.IsRunningOnStartup(App.GetApplicationPath());
-            }
+            runOnStartupCheckBox.IsChecked = TaskSchedulerUtils.IsRunningOnStartup(App.GetApplicationPath());
         }
+    }
 
-        private void hideButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-        }
+    private void hideButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Hide();
     }
 }
